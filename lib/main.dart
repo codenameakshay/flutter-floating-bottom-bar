99874704c5a31d13c10167bd530e3fae76bb3ba4
codenameakshay <akshaymaurya3006@gmail.com>
2021-12-26 20:29:21 +0530

    update example

--- lib/main.dart
+++ lib/main.dart
@@ -21,29 +21,169 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late int currentPage;
+  late TabController tabController;
+  final List<Color> colors = [
+    Colors.yellow,
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.pink
+  ];
+
+  @override
+  void initState() {
+    currentPage = 0;
+    tabController = TabController(length: 5, vsync: this);
+    tabController.animation!.addListener(
+      () {
+        final value = tabController.animation!.value.round();
+        if (value != currentPage && mounted) {
+          changePage(value);
+        }
+      },
+    );
+    super.initState();
+  }
+
+  void changePage(int newPage) {
+    setState(() {
+      currentPage = newPage;
+    });
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Color unselectedColor = colors[currentPage].computeLuminance() < 0.5
+        ? Colors.black
+        : Colors.white;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(widget.title),
           backgroundColor: Colors.black,
         ),
         body: BottomBar(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "This is the floating widget",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
+          child: TabBar(
+            indicatorPadding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+            controller: tabController,
+            indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(
+                    color: currentPage == 0
+                        ? colors[0]
+                        : currentPage == 1
+                            ? colors[1]
+                            : currentPage == 2
+                                ? colors[2]
+                                : currentPage == 3
+                                    ? colors[3]
+                                    : currentPage == 4
+                                        ? colors[4]
+                                        : unselectedColor,
+                    width: 4),
+                insets: EdgeInsets.fromLTRB(16, 0, 16, 8)),
+            tabs: [
+              SizedBox(
+                height: 55,
+                width: 40,
+                child: Center(
+                    child: Icon(
+                  Icons.home,
+                  color: currentPage == 0 ? colors[0] : unselectedColor,
+                )),
+              ),
+              SizedBox(
+                height: 55,
+                width: 40,
+                child: Center(
+                    child: Icon(
+                  Icons.search,
+                  color: currentPage == 1 ? colors[1] : unselectedColor,
+                )),
+              ),
+              SizedBox(
+                height: 55,
+                width: 40,
+                child: Center(
+                    child: Icon(
+                  Icons.add,
+                  color: currentPage == 2 ? colors[2] : unselectedColor,
+                )),
+              ),
+              SizedBox(
+                height: 55,
+                width: 40,
+                child: Center(
+                    child: Icon(
+                  Icons.favorite,
+                  color: currentPage == 3 ? colors[3] : unselectedColor,
+                )),
+              ),
+              SizedBox(
+                height: 55,
+                width: 40,
+                child: Center(
+                    child: Icon(
+                  Icons.settings,
+                  color: currentPage == 4 ? colors[4] : unselectedColor,
+                )),
+              ),
+            ],
+          ),
+          fit: StackFit.expand,
+          icon: Center(
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: null,
+              icon: Icon(
+                Icons.arrow_upward_rounded,
+                color: unselectedColor,
+              ),
             ),
           ),
-          body: (context, controller) =>
-              InfiniteListPage(controller: controller, color: Colors.blue),
+          borderRadius: BorderRadius.circular(500),
+          duration: Duration(seconds: 1),
+          curve: Curves.decelerate,
+          showIcon: true,
+          width: MediaQuery.of(context).size.width * 0.8,
+          barColor: colors[currentPage].computeLuminance() > 0.5
+              ? Colors.black
+              : Colors.white,
+          start: 2,
+          end: 0,
+          bottom: 10,
+          alignment: Alignment.bottomCenter,
+          iconHeight: 35,
+          iconWidth: 35,
+          reverse: false,
+          hideOnScroll: true,
+          scrollOpposite: false,
+          onBottomBarHidden: () {},
+          onBottomBarShown: () {},
+          body: (context, controller) => TabBarView(
+            controller: tabController,
+            dragStartBehavior: DragStartBehavior.down,
+            physics: const BouncingScrollPhysics(),
+            children: colors
+                .map((e) => InfiniteListPage(controller: controller, color: e))
+                .toList(),
+          ),
         ),
       ),
     );
