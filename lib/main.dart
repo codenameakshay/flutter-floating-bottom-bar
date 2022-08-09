6df0183b44707ca955ab507c70c3867e267faf36
codenameakshay <akshaymaurya3006@gmail.com>
2022-08-09 11:21:24 +0530

    update example to include clipping example

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     final Color unselectedColor = colors[currentPage].computeLuminance() < 0.5 ? Colors.black : Colors.white;
+    final Color unselectedColorReverse = colors[currentPage].computeLuminance() < 0.5 ? Colors.white : Colors.black;
 
     return SafeArea(
       child: Scaffold(
@@ -80,65 +81,79 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
           backgroundColor: Colors.black,
         ),
         body: BottomBar(
-          child: TabBar(
-            indicatorPadding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
-            controller: tabController,
-            indicator: UnderlineTabIndicator(
-                borderSide: BorderSide(
-                  color: currentPage <= 4 ? colors[currentPage] : unselectedColor,
-                  width: 4,
-                ),
-                insets: EdgeInsets.fromLTRB(16, 0, 16, 8)),
-            tabs: [
-              SizedBox(
-                height: 55,
-                width: 40,
-                child: Center(
-                    child: Icon(
-                  Icons.home,
-                  color: currentPage == 0 ? colors[0] : unselectedColor,
-                )),
-              ),
-              SizedBox(
-                height: 55,
-                width: 40,
-                child: Center(
-                  child: Icon(
-                    Icons.search,
-                    color: currentPage == 1 ? colors[1] : unselectedColor,
+          clip: Clip.none,
+          child: Stack(
+            alignment: Alignment.center,
+            clipBehavior: Clip.none,
+            children: [
+              TabBar(
+                indicatorPadding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                controller: tabController,
+                indicator: UnderlineTabIndicator(
+                    borderSide: BorderSide(
+                      color: currentPage <= 4 ? colors[currentPage] : unselectedColor,
+                      width: 4,
+                    ),
+                    insets: EdgeInsets.fromLTRB(16, 0, 16, 8)),
+                tabs: [
+                  SizedBox(
+                    height: 55,
+                    width: 40,
+                    child: Center(
+                        child: Icon(
+                      Icons.home,
+                      color: currentPage == 0 ? colors[0] : unselectedColor,
+                    )),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 55,
-                width: 40,
-                child: Center(
-                  child: Icon(
-                    Icons.add,
-                    color: currentPage == 2 ? colors[2] : unselectedColor,
+                  SizedBox(
+                    height: 55,
+                    width: 40,
+                    child: Center(
+                      child: Icon(
+                        Icons.search,
+                        color: currentPage == 1 ? colors[1] : unselectedColor,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 55,
-                width: 40,
-                child: Center(
-                  child: Icon(
-                    Icons.favorite,
-                    color: currentPage == 3 ? colors[3] : unselectedColor,
+                  SizedBox(
+                    height: 55,
+                    width: 40,
+                    child: Center(
+                      child: Icon(
+                        Icons.add,
+                        color: currentPage == 2 ? colors[2] : unselectedColorReverse,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 55,
-                width: 40,
-                child: Center(
-                  child: Icon(
-                    Icons.settings,
-                    color: currentPage == 4 ? colors[4] : unselectedColor,
+                  SizedBox(
+                    height: 55,
+                    width: 40,
+                    child: Center(
+                      child: Icon(
+                        Icons.favorite,
+                        color: currentPage == 3 ? colors[3] : unselectedColor,
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 55,
+                    width: 40,
+                    child: Center(
+                      child: Icon(
+                        Icons.settings,
+                        color: currentPage == 4 ? colors[4] : unselectedColor,
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              Positioned(
+                top: -25,
+                child: FloatingActionButton(
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                ),
+              )
             ],
           ),
           fit: StackFit.expand,
@@ -154,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
             ),
           ),
           borderRadius: BorderRadius.circular(500),
-          duration: Duration(milliseconds: 120),
+          duration: Duration(milliseconds: 500),
           curve: Curves.decelerate,
           showIcon: true,
           width: MediaQuery.of(context).size.width * 0.8,
